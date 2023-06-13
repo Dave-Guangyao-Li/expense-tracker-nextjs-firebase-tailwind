0f1ed78562d6c60b8045fb6620a1b745b8f70cea
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-06-13 11:07:38 -0700

    list item mapping and layout

--- app/page.js
+++ app/page.js
@@ -1,6 +1,17 @@
+'use client' // nextjs create server side rendered react app by default, this tells it to create a client side rendered app
+import React, {
+  useState, useEffect
+} from 'react'
 import Image from 'next/image'
 
 export default function Home() {
+  const [item, setItems] = useState([
+    { name: 'coffee', price: 4.95 },
+    { name: 'tea', price: 3.95 },
+    { name: 'scone', price: 2.95 },
+  ]);
+  const [total, setTotal] = useState(0);
+
   return (
     <main className="flex min-h-screen flex-col items-center sm:p-24 p-4">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
@@ -12,6 +23,18 @@ export default function Home() {
           <input className='col-span-2 p-3 border mx-3' type='number' placeholder='Enter $' />
           <button className='text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl' type='submit'>+</button>
         </form>
+
+        <ul>
+          {item.map((item, index) => (
+            <li key={index} className='flex justify-between my-4 w-full text-white bg-slate-950'>
+              <div className='p-4 w-full flex justify-between'>
+                <span className='capitalize'>{item.name}</span>
+                <span>${item.price}</span>
+              </div>
+              <button className='ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16'>X</button>
+            </li>
+          ))}
+        </ul>
       </div >
     </main >
   )
--- notes.md
+++ notes.md
@@ -1,7 +1,9 @@
 ```javascript
 npm i -g create-next-app
-npx create-next-app .
+npx create-next-app . // np typescript, no eslint, no prettier, with tailwindcss, and app routing, no src folder.
 
 // start up development server
 npm run dev
+
+'use client' // page.js nextjs create server side rendered react app by default, this tells it to create a client side rendered app
 ```
